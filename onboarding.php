3b0fe00d4f9121da6011015b55ca54dab6657c00
hailm <hai.lemanh.1310@gmail.com>
2022-11-07 13:03:30 +0700

    update

---
+++ onboarding.php
@@ -0,0 +1,74 @@
+<?php $data["title"] = "Onboarding"; ?>
+<?php $bodyClass = ''; ?>
+<?php $isMenu = true; ?>
+<?php require "template-parts/layouts/header.php"; ?>
+<div class="onboarding uk-section-xsmall" uk-height-viewport="offset-bottom: true">
+    <div class="uk-container">
+        <h1 class="onboarding__title uk-h1 uk-margin-remove">Xin chào Giác sơ đồ,</h1>
+        <div class="item__30 onboarding__box1 onboarding__box1--doing">
+            <div class="uk-flex-middle uk-grid-small" uk-grid>
+                <div class="uk-width-auto">
+                    <div class="onboarding__box1__coverImg uk-cover-container uk-background-muted uk-border-rounded uk-background-norepeat uk-background-center-center" data-src="images/Frame1.png" uk-img>
+                        <canvas width="64" height="64"></canvas>
+                    </div>
+                </div>
+                <div class="uk-width-expand">
+                    <h4 class="uk-h4 onboarding__box1__title">Mẫu đang sản xuất</h4>
+                </div>
+            </div>
+        </div>
+        <div class="item__24 onboarding__box1 onboarding__box1--finish">
+            <div class="uk-flex-middle uk-grid-small" uk-grid>
+                <div class="uk-width-auto">
+                    <div class="onboarding__box1__coverImg uk-cover-container uk-background-muted uk-border-rounded uk-background-norepeat uk-background-center-center" data-src="images/Frame.png" uk-img>
+                        <canvas width="64" height="64"></canvas>
+                    </div>
+                </div>
+                <div class="uk-width-expand">
+                    <h4 class="uk-h4 onboarding__box1__title">Mẫu đã hoàn thành</h4>
+                </div>
+            </div>
+        </div>
+        <div class="item__24 onboarding__box2">
+            <div class="uk-grid-collapse uk-child-width-expand" uk-grid>
+                <?php
+                $data = array(
+                    array(
+                        'class' => 'late',
+                        'icon' => 'clock',
+                        'txt' => 'Trễ',
+                        'count' => '10',
+                    ),
+                    array(
+                        'class' => 'warning',
+                        'icon' => 'clock',
+                        'txt' => 'Cảnh báo',
+                        'count' => '10',
+                    ),
+                    array(
+                        'class' => 'doing',
+                        'icon' => 'clock',
+                        'txt' => 'Đang làm',
+                        'count' => '4',
+                    ),
+                    array(
+                        'class' => 'waiting',
+                        'icon' => 'clock',
+                        'txt' => 'Chờ xử lý',
+                        'count' => '9',
+                    ),
+                );
+                foreach ($data as $k=>$v): ?>
+                <div>
+                    <div class="uk-flex uk-flex-column uk-flex-middle <?= $v['class'] ?>">
+                        <span class="uk-icon-button onboarding__box2__icon" uk-icon="<?= $v['icon'] ?>"></span>
+                        <div class="item__8 onboarding__box2__txt"><?= $v['txt'] ?></div>
+                        <div class="item__4 onboarding__box2__txt <?= $v['class'] ?>"><?= $v['count'] ?></div>
+                    </div>
+                </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require "template-parts/layouts/footer.php"; ?>
--- template-parts/layouts/footer.php
+++ template-parts/layouts/footer.php
@@ -1,4 +1,104 @@
+<?php if ($isMenu): ?>
+<nav class="kanns-navbar uk-navbar-container uk-position-fixed uk-position-bottom" uk-navbar>
+    <div class="uk-navbar-left uk-flex-1">
+
+        <ul class="uk-navbar-nav uk-flex-1 uk-child-width-expand uk-grid-match">
+            <li class="uk-active">
+                <a href="#">
+                    <div class="uk-flex uk-flex-column uk-flex-middle">
+                        <span class="kanns-navbar__iconNav kanns-navbar__iconNav--home"></span>
+                        <div class="uk-navbar-subtitle kanns-navbar__subtitle item__2">Trang chủ</div>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="uk-flex uk-flex-column uk-flex-middle">
+                        <span class="kanns-navbar__iconNav kanns-navbar__iconNav--job"></span>
+                        <div class="uk-navbar-subtitle kanns-navbar__subtitle item__2">Công việc</div>
+                    </div>
+                </a>
+            </li>
+
+            <li>
+                <a href="" class="uk-position-relative uk-position-z-index">
+                    <span class="kanns-navbar__iconPlus uk-border-circle uk-flex uk-position-z-index-negative" uk-icon="icon: plus-circle; ratio: 1"></span>
+                </a>
+                <div class="kanns-navbar__dropdown" hidden style="" uk-drop="mode: click; pos: top-center;">
+                    <div class="uk-child-width-1-2 uk-grid-12 uk-grid-match" uk-grid>
+                        <div>
+                            <div class="kanns-navbar__dropdown__item">
+                                <span class="uk-margin-small-right kanns-navbar__dropdown__icon kanns-navbar__dropdown__icon--note" uk-icon="check"></span>
+                                Notepad
+                            </div>
+                        </div>
+                        <div>
+                            <div class="kanns-navbar__dropdown__item">
+                                <span class="uk-margin-small-right kanns-navbar__dropdown__icon kanns-navbar__dropdown__icon--reminder" uk-icon="check"></span>
+                                Reminder
+                            </div>
+                        </div>
+                        <div class="uk-width-1-1">
+                            <div class="kanns-navbar__dropdown__item">
+                                <span class="uk-margin-small-right kanns-navbar__dropdown__icon kanns-navbar__dropdown__icon--add" uk-icon="check"></span>
+                                Tạo mẫu mới
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mask_menu uk-position-z-index-zero" style="display: none"></div>
+            </li>
+
+            <li>
+                <a href="#">
+                    <div class="uk-flex uk-flex-column uk-flex-middle">
+                        <span class="kanns-navbar__iconNav kanns-navbar__iconNav--notify"></span>
+                        <div class="uk-navbar-subtitle kanns-navbar__subtitle item__2">Thông báo</div>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="uk-flex uk-flex-column uk-flex-middle">
+                        <span class="kanns-navbar__iconNav kanns-navbar__iconNav--profile"></span>
+                        <div class="uk-navbar-subtitle kanns-navbar__subtitle item__2">Profile</div>
+                    </div>
+                </a>
+            </li>
+        </ul>
+
+    </div>
+</nav>
+<?php endif; ?>
 </div>
 <!--/app-->
+<script>
+    const $ = document.querySelector.bind(document);
+    const $$ = document.querySelectorAll.bind(document);
+
+    const app = {
+        render: function () {
+            const kanns_navbar = $('.kanns-navbar');
+            const app = $('#app');
+            const kanns_navbarHeight = kanns_navbar ? kanns_navbar.offsetHeight + 'px' : 0;
+            app.style.paddingBottom = kanns_navbarHeight;
+            console.log('paddingBottom App',kanns_navbarHeight);
+
+            UIkit.util.on('.kanns-navbar__dropdown', 'show', function () {
+                // do something
+                $('.mask_menu').style.display = 'block';
+            });
+
+            UIkit.util.on('.kanns-navbar__dropdown', 'hide', function () {
+                // do something
+                $('.mask_menu').style.display = 'none';
+            });
+        },
+        start: function () {
+            this.render();
+        }
+    }
+    app.start();
+</script>
 </body>
 </html>
